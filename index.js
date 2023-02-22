3b15f6405bda0ae2a29093de953d16b41aed7797
Doni Nurramdan <doni.nurramdan@indocyber.id>
2023-02-22 14:29:48 +0700

    add openai library

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const request = require('request');
 const bodyParser = require('body-parser');
 const app = express();
 
-const CHATGPT_API_URL = 'https://api.openai.com/v1/engines/davinci-codex/completions';
-const CHATGPT_API_KEY = 'YOUR_CHATGPT_API_KEY';
+// const CHATGPT_API_URL = 'https://api.openai.com/v1/engines/davinci-codex/completions';
+// const CHATGPT_API_URL = 'https://api.example.com/chatgpt';
+const CHATGPT_API_URL = 'https://chat.openai.com/chat';
+const CHATGPT_API_KEY = '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';
 
 app.use(bodyParser.json());
 
@@ -31,9 +33,10 @@ app.post('/chatgpt', (req, res) => {
             console.log(error);
             res.status(500).json({ error: 'Internal Server Error' });
         } else {
-            const { choices } = JSON.parse(body).choices[0];
-            const botResponse = choices[0].text.trim().split('Bot:')[1].trim();
-            res.json({ message: botResponse });
+            // const { choices } = JSON.parse(body).choices[0];
+            // const botResponse = choices[0].text.trim().split('Bot:')[1].trim();
+            // res.json({ message: botResponse });
+            res.json({ messageReply: body });
         }
     });
 });
